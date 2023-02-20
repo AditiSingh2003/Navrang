e075a0c1814986d7c28f497daf14a517c8ec94bc
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-02-20 22:30:32 +0530

    Okay

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -38,9 +38,10 @@ export default function LandingPage() {
             <Text>Navrang</Text>
             <Text mb={100}>The Cultural Color Of PSIT</Text>
 
-            <Box className="page-content" >
+            <Box className="page-content">
               <Text fontSize={["", "", "md"]} m="4" ml={"0"} mb={100}>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Praesentium
               </Text>
               <Button
                 bgColor={"black"}
@@ -48,12 +49,10 @@ export default function LandingPage() {
                 mt={"8"}
                 _hover={{ bgColor: "rgba(0, 0, 0, 0.64)" }}
               >
-              Know More
+                Know More
               </Button>
             </Box>
           </Box>
-
-          
         </Box>
 
         <Box
@@ -62,54 +61,20 @@ export default function LandingPage() {
           display="flex"
           flexDirection={"column"}
         >
-          <Box display={"flex"} justifyContent="center" alignContent={"center"} width={'500px'}>
+          <Box
+            display={"flex"}
+            justifyContent="center"
+            alignContent={"center"}
+            width={"500px"}
+          >
             <Image
+              boxShadow={
+                "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px"
+              }
               src="/banner.png"
-         
-              m={["", "", "10", "0"]}
-              p={["", "", "10", "0"]}
+              borderRadius={"3xl"}
+              ml={"44"}
             />
-          </Box>
-          <Box display={["none", "", "flex"]} justifyContent="flex-end">
-            <Box
-              m={["", "", "10", "10"]}
-              bgColor="#E5D1FA"
-              p={"3"}
-              borderRadius="full"
-              position={"fixed"}
-              _hover={{ cursor: "pointer"}}
-            >
-              <Modal
-                initialFocusRef={initialRef}
-                finalFocusRef={finalRef}
-                isOpen={isOpen}
-                onClose={onClose}
-              >
-                <ModalOverlay />
-                <ModalContent>
-                  <ModalHeader>Contact Us</ModalHeader>
-                  <ModalCloseButton />
-                  <ModalBody pb={6}>
-                    <FormControl>
-                      <FormLabel>Your name</FormLabel>
-                      <Input ref={initialRef} placeholder="Full name" />
-                    </FormControl>
-
-                    <FormControl mt={4}>
-                      <FormLabel>Your Message</FormLabel>
-                      <Input placeholder="Enter your Message" size={"lg"} />
-                    </FormControl>
-                  </ModalBody>
-
-                  <ModalFooter>
-                    <Button colorScheme="blue" mr={3}>
-                      Submit
-                    </Button>
-                    <Button onClick={onClose}>Cancel</Button>
-                  </ModalFooter>
-                </ModalContent>
-              </Modal>
-            </Box>
           </Box>
         </Box>
       </Box>
