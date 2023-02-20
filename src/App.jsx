88ea0f5ad71c71f85914d0f71e5b4c6bed4991d0
Aarav238 <aarav8090shukla@gmail.com>
2023-02-20 21:54:29 +0530

    okkk

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import './App.css'
 import Events from './components/Events'
 
 import LandingPage from './components/Main'
-
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
 
 
 function App() {
-
   return (
     <div className="App">
       <Navbar/>
+      <LandingPage/>
       <Events />
+      <Footer/>
     </div>
   )
 }
+
+
 
 export default App
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -126,12 +126,12 @@ export default function LandingPage() {
           p={["12", "24", "24", "24"]}
           pl={["", "", "20", "20"]}
         >
-          <Text>With the Right Software, Great Things Can Happen</Text>
-
           <Text>Club Ambassador Message </Text>
 
           <Text>Campus Ambassador</Text>
- 259c6e47f25505d11c305e2084b04dd7b9adcee9
+        
+
+ 
         </Box>
 
         <Box p={["12", "24", "24", "24"]}>
